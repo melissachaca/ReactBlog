e0e58d7a08c5d1b49c9f432f6d1fb5e77b623e6e
Melissa Chaca <chacamelissad@gmail.com>
2022-02-07 10:39:10 -0600

    fetch link edit

--- src/views/Posts.js
+++ src/views/Posts.js
@@ -5,7 +5,7 @@ import PostCard from '../components/PostCard';
 export default function Post(props){
     const [posts, setPost] = useState([]);
     useEffect(() => {
-        fetch('https://kekambas-blog.herokuapp.com/')
+        fetch('https://kekambas-blog.herokuapp.com/blog/posts')
             .then(res => res.json())
             .then(data => {
                 console.log(data);
